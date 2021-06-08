abf32644dcd15f8129df1abd52b0c7443d784360
Jason carrazco <carrazcojason10@gmail.com>
2021-06-07 21:41:50 -0500

    setting nav index

---
+++ midnight/src/components/Nav/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+import { Navbar, Nav } from "react-bootstrap";
+
+function NavReturn() {
+  return (
+    <div style={{ paddingTop: "75px" }}>
+      <Navbar className={"Nav"} fixed="top" expand="lg">
+        <Navbar.Brand href="/">Jason A Carrazco</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavReturn;
---
+++ midnight/src/components/Repos/index.js
@@ -0,0 +1,3 @@
+import React, { useEffect, useState } from "react"
+import "./style.css";
+import {repos} from "../../utils/API";
