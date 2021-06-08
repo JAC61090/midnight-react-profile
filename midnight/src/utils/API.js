06c1811b9ef1a34d3b257c400fccd896f2168fce
Jason carrazco <carrazcojason10@gmail.com>
2021-06-07 21:19:32 -0500

    setting axios for github

--- midnight/src/utils/API.js
+++ midnight/src/utils/API.js
@@ -1,1 +1,5 @@
 import axios from "axios";
+
+
+export let profileInfo =  axios.get("https://api.github.com/users/JAC61090")
+export let repos =  axios.get("https://api.github.com/users/JAC61090/repos")
