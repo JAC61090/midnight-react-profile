53dc7fe48e284a30e98a0e75440c874821816c84
Jason carrazco <carrazcojason10@gmail.com>
2021-06-07 21:54:48 -0500

    function for table sort

--- midnight/src/components/Repos/index.js
+++ midnight/src/components/Repos/index.js
@@ -1,3 +1,42 @@
 import React, { useEffect, useState } from "react"
 import "./style.css";
 import {repos} from "../../utils/API";
+
+const useSortableData = (items, config = null) => {
+
+    // console.log(items);
+    const [sortConfig, setSortConfig] = React.useState(config);
+  
+  
+  
+    const sortedItems = React.useMemo(() => {
+      let sortableItems = [...items];
+      if (sortConfig !== null) {
+        sortableItems.sort((a, b) => {
+          if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? -1 : 1;
+          }
+          if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? 1 : -1;
+          }
+          return 0;
+        });
+      }
+      return sortableItems;
+    }, [items, sortConfig]);
+  
+    const requestSort = (key) => {
+      let direction = 'ascending';
+      if (
+        sortConfig &&
+        sortConfig.key === key &&
+        sortConfig.direction === 'ascending'
+      ) {
+        direction = 'descending';
+      }
+      setSortConfig({ key, direction });
+    };
+  
+    return { items: sortedItems, requestSort, sortConfig };
+};
+
