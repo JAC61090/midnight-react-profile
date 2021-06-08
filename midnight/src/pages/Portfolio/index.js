9135bec57019a29ee102f8dd7ed6d4c58470528f
Jason carrazco <carrazcojason10@gmail.com>
2021-06-07 21:16:10 -0500

    setting api context

---
+++ midnight/src/pages/Portfolio/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Projects from "../../components/Repos/index";
+
+function Portfolio() {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <Projects />
+    </div>
+  );
+}
+
+export default Portfolio;
