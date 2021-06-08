d899b570ddc57c58a3f051817edc1edefae5fdeb
Jason carrazco <carrazcojason10@gmail.com>
2021-06-07 21:33:23 -0500

    updating footer

---
+++ midnight/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ModalFooter } from "react-bootstrap";
+
+function FootReturn() {
+  return (
+    <div>
+      <ModalFooter bg="light" expand="lg">
+        Salutations!
+      </ModalFooter>
+    </div>
+  );
+}
+
+export default FootReturn;
